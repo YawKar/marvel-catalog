cb7f5d5bb13cd57e5ca68fe4627a38f4c1973ba2
Yawkar <yawkarpov@gmail.com>
2022-07-07 23:23:01 +0300

    - 1st (0-based) commit

---
+++ src/test/java/com/yawkar/marvelcatalog/MarvelCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yawkar.marvelcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarvelCatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
