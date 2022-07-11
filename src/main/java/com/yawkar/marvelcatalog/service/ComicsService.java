5d4446c0c54ee6028c8e0c9139ad7c9cb075e46e
Yawkar <yawkarpov@gmail.com>
2022-07-11 17:05:22 +0300

    - Add ModelMapperConfiguration - Add Bean of ModelMapper to ModelMapperConfiguration - Inject ModelMapper into services

--- src/main/java/com/yawkar/marvelcatalog/service/ComicsService.java
+++ src/main/java/com/yawkar/marvelcatalog/service/ComicsService.java
@@ -1,14 +1,17 @@
 package com.yawkar.marvelcatalog.service;
 
 import com.yawkar.marvelcatalog.repository.ComicsRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ComicsService {
 
     private final ComicsRepository comicsRepository;
+    private final ModelMapper modelMapper;
 
-    public ComicsService(ComicsRepository comicsRepository) {
+    public ComicsService(ComicsRepository comicsRepository, ModelMapper modelMapper) {
         this.comicsRepository = comicsRepository;
+        this.modelMapper = modelMapper;
     }
 }
--- src/main/java/com/yawkar/marvelcatalog/service/HeroesService.java
+++ src/main/java/com/yawkar/marvelcatalog/service/HeroesService.java
@@ -1,14 +1,17 @@
 package com.yawkar.marvelcatalog.service;
 
 import com.yawkar.marvelcatalog.repository.HeroesRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HeroesService {
 
     private final HeroesRepository heroesRepository;
+    private final ModelMapper modelMapper;
 
-    public HeroesService(HeroesRepository heroesRepository) {
+    public HeroesService(HeroesRepository heroesRepository, ModelMapper modelMapper) {
         this.heroesRepository = heroesRepository;
+        this.modelMapper = modelMapper;
     }
 }
