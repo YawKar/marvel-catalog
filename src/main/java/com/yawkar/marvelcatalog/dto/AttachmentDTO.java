4e7d387076fb5c60fcd33152c24e98257ae0120c
Yawkar <yawkarpov@gmail.com>
2022-07-12 14:59:29 +0300

    - Add `SwaggerConfig` with OpenAPI definition bean - Add `Schema` descriptions in DTOs

--- src/main/java/com/yawkar/marvelcatalog/dto/AttachmentDTO.java
+++ src/main/java/com/yawkar/marvelcatalog/dto/AttachmentDTO.java
@@ -1,17 +1,21 @@
 package com.yawkar.marvelcatalog.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.List;
 
+@Schema(description = "An attachment entity")
 public class AttachmentDTO {
 
+    @Schema(description = "List of hero_ids that should be connected", example = "[1, 42, 777]")
     @JsonProperty("hero_ids")
     @NotNull(message = "The hero_ids list can't be null")
     @Size(min = 1, message = "The hero_ids list should contain at least one id")
     private List<Long> heroIds;
+    @Schema(description = "List of comic_ids that should be connected", example = "[1, 2, 3]")
     @JsonProperty("comic_ids")
     @NotNull(message = "The comic_ids list can't be null")
     @Size(min = 1, message = "The comic_ids list should contain at least one id")
--- src/main/java/com/yawkar/marvelcatalog/dto/HeroDTO.java
+++ src/main/java/com/yawkar/marvelcatalog/dto/HeroDTO.java
@@ -1,19 +1,25 @@
 package com.yawkar.marvelcatalog.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
+@Schema(description = "A hero entity")
 public class HeroDTO {
 
+    @Schema(description = "Internal identification number of an entity")
     private long id;
+    @Schema(description = "The real name of a hero", example = "Clark Kent")
     @JsonProperty("real_name")
     @NotBlank(message = "Name can't be blank or null")
     private String realName;
+    @Schema(description = "The alias of a hero", example = "Superman")
     @NotBlank(message = "Alias can't be blank or null")
     private String alias;
+    @Schema(description = "Superpowers of a hero", example = "['Laser beam eyes', 'Super strength']")
     @NotNull(message = "The superpowers list can't be null")
     private List<String> superpowers;
 
