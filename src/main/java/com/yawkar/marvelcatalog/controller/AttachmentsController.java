1511d84c147d092289ec36b7f5112c259790b76b
Yawkar <yawkarpov@gmail.com>
2022-07-12 17:13:38 +0300

    - Add Swagger documentation annotations to `AttachmentsController` - Add Swagger documentation annotations to `CharactersController` - Add Swagger documentation annotations to `ComicsController`

--- src/main/java/com/yawkar/marvelcatalog/controller/AttachmentsController.java
+++ src/main/java/com/yawkar/marvelcatalog/controller/AttachmentsController.java
@@ -2,6 +2,10 @@ package com.yawkar.marvelcatalog.controller;
 
 import com.yawkar.marvelcatalog.dto.AttachmentDTO;
 import com.yawkar.marvelcatalog.service.AttachmentsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+@Tag(name = "Attachment controller", description = "Contains one PUT method for connecting/joining comics and heroes")
 @RestController
 @RequestMapping("/v1/public/attach")
 public class AttachmentsController {
@@ -20,8 +25,25 @@ public class AttachmentsController {
         this.attachmentsService = attachmentsService;
     }
 
+    @Operation(
+            summary = "Attaches heroes to comics",
+            description = "Adds connections between heroes and comics"
+    )
+    @ApiResponses(
+            {
+                @ApiResponse(
+                        responseCode = "200",
+                        description = "Successfully updated connections"
+
+                ),
+                @ApiResponse(
+                        responseCode = "404",
+                        description = "Some ids don't exist in catalog"
+                )
+            }
+    )
     @PutMapping
-    public ResponseEntity<Object> attachComicsToHeroes(@Valid @RequestBody AttachmentDTO attachmentDTO) {
+    public ResponseEntity<Void> attachComicsToHeroes(@Valid @RequestBody AttachmentDTO attachmentDTO) {
         attachmentsService.attachComicsToHeroes(attachmentDTO);
         return ResponseEntity.ok().build();
     }
